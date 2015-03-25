f58a3bd6afbd2c43f4bbc8d9439a3a3f18aaa469
Erik Vold <evold@mozilla.com>
2015-03-24 20:14:50 -0700

    Adding a package.json and some test files

---
+++ test/test.patches.js
@@ -0,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+var path = require("path");
+var fs = require("fs");
+
+var patch = require("../");
+
+describe("test patches", function () {
+  it("test patch 1", function (done) {
+    var chunks = patch.getChunks();
+  });
+});
